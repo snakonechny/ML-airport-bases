689e33372e413ac4fbb0fe50e070cca753ee38c9
snakonechny <svyatnakonechny@gmail.com>
2016-01-13 19:29:46 -0600

    second commit

--- ML-airport-bases.R
+++ ML-airport-bases.R
@@ -1,17 +1,26 @@
 library(dplyr)
 
-file.list <- list.files(pattern = '.csv')
+file.list <- as.list(list.files(pattern = '.csv'))
 
 test <- read.csv('12-2014.csv', header = TRUE)
 head(test)
 
 master <- data.frame()
 
-for (i in file.list) {
+system.time(for (i in file.list) {
   temp <- read.csv(i, header = TRUE)
   temp <- temp %>% filter(UNIQUE_CARRIER == 'UA')
   
   master <- bind_rows(master, temp)
+})
+
+loader <- function(i) {
+  temp <- read.csv(i, header = TRUE)
+  temp <- temp %>% filter(UNIQUE_CARRIER == 'UA')
+  master <- bind_rows(master, temp)
+  return(master)
 }
+
+proc.time(lapply(X = file.list, FUN = loader))
 
 write.csv(master, 'master.csv')
